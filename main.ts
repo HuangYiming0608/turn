1250187447bf459dbd4e09c3e990c533d53db810
HuangYiming0608 <133227329+HuangYiming0608@users.noreply.github.com>
2024-05-08 16:17:47 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 basic.forever(function () {
     if (CrocoKit_Sensor.Ultrasonic(DigitalPin.P8, DigitalPin.P9) < 10) {
-        if (CrocoKit_Sensor.Ultrasonic(DigitalPin.P6, DigitalPin.P7) < CrocoKit_Sensor.Ultrasonic(DigitalPin.P1, DigitalPin.P2)) {
+        if (CrocoKit_Sensor.Ultrasonic(DigitalPin.P6, DigitalPin.P7) > CrocoKit_Sensor.Ultrasonic(DigitalPin.P1, DigitalPin.P2)) {
             basic.showLeds(`
                 . . # . .
                 . # . . .
