a3f4df45f2d117db8e52fe89d8b6070bf32c39e6
HuangYiming0608 <133227329+HuangYiming0608@users.noreply.github.com>
2024-05-15 16:36:27 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+input.onButtonPressed(Button.A, function () {
+    datalogger.deleteLog(datalogger.DeleteType.Full)
+})
 basic.forever(function () {
     if (CrocoKit_Sensor.Ultrasonic(DigitalPin.P8, DigitalPin.P9) < 5) {
         if (CrocoKit_Sensor.Ultrasonic(DigitalPin.P6, DigitalPin.P7) > CrocoKit_Sensor.Ultrasonic(DigitalPin.P1, DigitalPin.P2)) {
@@ -8,6 +11,7 @@ basic.forever(function () {
                 . # . . .
                 . . # . .
                 `)
+            datalogger.log(datalogger.createCV("l", 1))
         } else {
             basic.showLeds(`
                 . . # . .
@@ -16,6 +20,7 @@ basic.forever(function () {
                 . . . # .
                 . . # . .
                 `)
+            datalogger.log(datalogger.createCV("r", 1))
         }
     } else {
         basic.showLeds(`
