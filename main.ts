a5922ec59b21c77e3d576371971b5900425a86d0
HuangYiming0608 <133227329+HuangYiming0608@users.noreply.github.com>
2024-05-15 16:39:17 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ datalogger.onLogFull(function () {
 input.onButtonPressed(Button.A, function () {
     datalogger.deleteLog(datalogger.DeleteType.Full)
 })
-datalogger.mirrorToSerial(true)
+datalogger.deleteLog(datalogger.DeleteType.Full)
 basic.forever(function () {
     if (CrocoKit_Sensor.Ultrasonic(DigitalPin.P8, DigitalPin.P9) < 5) {
         if (CrocoKit_Sensor.Ultrasonic(DigitalPin.P6, DigitalPin.P7) > CrocoKit_Sensor.Ultrasonic(DigitalPin.P1, DigitalPin.P2)) {
